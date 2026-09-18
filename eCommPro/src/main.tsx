d93a13e0c4ca80bff3943a427072c0493623cef5
Jesús Sir <sirjesus9@gmail.com>
2026-09-18 05:15:28 -0300

    cambios en main.tsx

--- eCommPro/src/main.tsx
+++ eCommPro/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
+import { AuthProvider } from './context/AuthContext'
+import { ProductosProvider } from './context/ProductContext'
+import { CarritoProvider } from './context/CarritoContext'
+import { DeseosProvider } from './context/DeseosContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ProductosProvider>
+        <CarritoProvider>
+          <DeseosProvider>
+            <App />
+          </DeseosProvider>
+        </CarritoProvider>
+      </ProductosProvider>
+    </AuthProvider>
   </StrictMode>,
 )
