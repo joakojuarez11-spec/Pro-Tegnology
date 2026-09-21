97bde0a43f488e7f57aa422401204a3d51da7175
Joaquin <joakojuarez11@gmail.com>
2026-09-21 01:06:12 -0300

    import nabvar

---
+++ src/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import { useState, useRef, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Search, User, Heart, ShoppingCart, Home, Menu, X, ChevronDown, ChevronUp, Shield, Info } from 'lucide-react';
+import { useCarrito } from '../context/CarritoContext';
+import { useAuth } from '../context/AuthContext';
+import { useProductos } from '../context/ProductosContext';
+import logoImg from '../assets/logo.png';
+import nameImg from '../assets/name.png';
+import fondoHero from '../assets/fondo-hero.jpg
